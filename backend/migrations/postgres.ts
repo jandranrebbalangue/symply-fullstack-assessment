79921546489119e106386ce4d90b16d770b10e69
Jan Dranreb Balangue <jandranrebbalangue@gmail.com>
2024-02-14 15:52:26 +0800

    Add migrations

---
+++ backend/migrations/postgres.ts
@@ -0,0 +1,23 @@
+import { Kysely, sql } from "kysely"
+
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createTable("tasks")
+    .addColumn("id", "serial", (col) => col.primaryKey())
+    .addColumn("name", "varchar", (col) => col.notNull())
+    .addColumn("status", "varchar")
+    .addColumn("created_at", "timestamp", (col) =>
+      col.defaultTo(sql`now()`).notNull()
+    )
+    .addColumn("updated_at", "timestamp", (col) =>
+      col.defaultTo(sql`now()`).notNull()
+    )
+    .addColumn("deleted_at", "timestamp", (col) =>
+      col.defaultTo(sql`now()`).notNull()
+    )
+    .execute()
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropTable("tasks").execute()
+}
