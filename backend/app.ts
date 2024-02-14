a1764be3f268d675d850ceece19e3837c7a82a63
Jan Dranreb Balangue <jandranrebbalangue@gmail.com>
2024-02-14 18:52:06 +0800

    Add cors

--- backend/app.ts
+++ backend/app.ts
@@ -4,6 +4,7 @@ import fastifyJwt from "@fastify/jwt"
 import { Type } from "@sinclair/typebox"
 import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox"
 import dotenv from "dotenv"
+import cors from "@fastify/cors"
 import { Tasks } from "./routes"
 dotenv.config()
 
@@ -13,6 +14,16 @@ const app = fastify<http.Server, http.IncomingMessage>({
 
 app.register(fastifyJwt, {
   secret: process.env.JWT_SECRET as string
+})
+app.register(cors, {
+  origin: (origin, cb) => {
+    const hostname = new URL(origin as string).hostname
+    if (hostname === "localhost") {
+      cb(null, true)
+      return
+    }
+    cb(new Error("Not allowed"), false)
+  }
 })
 
 app.post(
